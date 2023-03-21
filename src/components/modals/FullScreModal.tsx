ed9be5f361b7c7104b5e811132906076eba5e959
badahertz52 <badahertz52@gmail.com>
2023-03-21 20:57:11 +0900

    #12 [Fix] Resolve issue of screen flickering caused by delayed compilation of product image files and CSS files in FullScreModal

--- src/components/modals/FullScreModal.tsx
+++ src/components/modals/FullScreModal.tsx
@@ -1,4 +1,4 @@
-import React, { MouseEvent, TouchEvent, useEffect, useState } from "react";
+import React, { MouseEvent, TouchEvent, useEffect, useRef, useState } from "react";
 import ModalPortal from "./ModalPortal";
 import { fullScreModalType} from "./modalTypes";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -9,6 +9,7 @@ type FullScreModalProps ={
 };
 const FullScreModal=({modalState ,closeModal}:FullScreModalProps)=>{
   const [saved,setSaved]= useState<boolean>(modalState.saved);
+  const divRef= useRef<HTMLDivElement>(null);
   const onClickSaveBtn =()=>{
     if(saved){
       //  상품 저장 취소
@@ -23,89 +24,101 @@ const FullScreModal=({modalState ,closeModal}:FullScreModalProps)=>{
     target?.classList.toggle("hover");
   };
   useEffect(()=>{
-    const modalEle = document.querySelector(".modal");
+    const modalEle = document.querySelector(".modal") as HTMLElement|null;
     modalEle?.classList.add("full-scre-modal");
+    setTimeout(() => {
+      if(divRef.current !==null){
+        divRef.current.style.display = "block"
+      }
+    }, 100);
+    return ()=>{
+      if(divRef.current !==null){
+        divRef.current.style.display = "none"
+      }
+    }
   },[]);
   return ( 
     <ModalPortal>
-      <div className="topbar">
-        <button
-          type="button"
-          title="previous"
-          onClick ={closeModal}
-          className ="btn-close"
-        >
-          <FontAwesomeIcon icon={faChevronLeft} />
-        </button>
-        <h2>상세 페이지</h2>
-      </div>
-      <div className="contents">
-        <img 
-          src={modalState.img} 
-          alt="product image" 
-          className="product-img"
-        />
-        <div className="not-img">
+      <div className="full-scre-modal-outbox" ref={divRef} style={{display:"none"}}>
+        <div className="topbar">
           <button
-              type="button"
-              title="save btn"
-              className={`btn-save ${saved? "on" : ""}`}
-              onClick ={onClickSaveBtn}
-            >
-              <FontAwesomeIcon icon={faHeart} />
+            type="button"
+            title="previous"
+            onClick ={closeModal}
+            className ="btn-close"
+          >
+            <FontAwesomeIcon icon={faChevronLeft} />
           </button>
-          <section className="inform">
-            <ul>
-              <li className="product-name">  
-                <span>{modalState.name}</span>
-              </li>
-              <li className="product-price">
-                <span>{`${modalState.price}원`}</span>
-              </li>
-              <li 
-                className="one-line-introduction"
+          <h2>상세 페이지</h2>
+        </div>
+        <div className="contents">
+          <img 
+            src={modalState.img} 
+            alt="product image" 
+            className="product-img"
+          />
+          <div className="not-img">
+            <button
+                type="button"
+                title="save btn"
+                className={`btn-save ${saved? "on" : ""}`}
+                onClick ={onClickSaveBtn}
               >
-                <span>
-                  {modalState.oneLineIntroduction}
-                </span>
-              </li>
-              <li className="tag">
-                {modalState.tag.map((v,i) => 
-                  <span
-                    key ={`${modalState.name}_tag${i}`}
-                  >
-                    #{v}
-                  </span>
-                    )}
-              </li>
-            </ul>
-          </section>
-          <section className="store">
-            <div className="online-store">
-              <h3>온라인 구매처 정보</h3>
-              {modalState.store.online.map((v,i)=>
-              <div className="site" key={`site_${i}`} >
-                <div className="site-name">
-                  {v.name}
-                </div>
-                <a 
-                  className="site-link"
-                  href={v.url}
-                  target="_blank"
-                  onMouseMove={(event)=>onMoveLink(event)}
-                  onTouchMove={(event)=>onMoveLink(event)}
+                <FontAwesomeIcon icon={faHeart} />
+            </button>
+            <section className="inform">
+              <ul>
+                <li className="product-name">  
+                  <span>{modalState.name}</span>
+                </li>
+                <li className="product-price">
+                  <span>{`${modalState.price}원`}</span>
+                </li>
+                <li 
+                  className="one-line-introduction"
                 >
-                  상품 보러 가기
-                </a>
-              </div>)}
-            </div>
-            <div className="offline-store">
-              <h3>오프라인 구매처 정보</h3>
-              <span>
-                {modalState.store.offline}
-              </span>
-            </div>
-          </section>
+                  <span>
+                    {modalState.oneLineIntroduction}
+                  </span>
+                </li>
+                <li className="tag">
+                  {modalState.tag.map((v,i) => 
+                    <span
+                      key ={`${modalState.name}_tag${i}`}
+                    >
+                      #{v}
+                    </span>
+                      )}
+                </li>
+              </ul>
+            </section>
+            <section className="store">
+              <div className="online-store">
+                <h3>온라인 구매처 정보</h3>
+                {modalState.store.online.map((v,i)=>
+                <div className="site" key={`site_${i}`} >
+                  <div className="site-name">
+                    {v.name}
+                  </div>
+                  <a 
+                    className="site-link"
+                    href={v.url}
+                    target="_blank"
+                    onMouseMove={(event)=>onMoveLink(event)}
+                    onTouchMove={(event)=>onMoveLink(event)}
+                  >
+                    상품 보러 가기
+                  </a>
+                </div>)}
+              </div>
+              <div className="offline-store">
+                <h3>오프라인 구매처 정보</h3>
+                <span>
+                  {modalState.store.offline}
+                </span>
+              </div>
+            </section>
+          </div>
         </div>
       </div>
     </ModalPortal>
