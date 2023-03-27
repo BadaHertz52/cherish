795b98956ef9af7dbde467957bcdda843a7ff868
badahertz52 <badahertz52@gmail.com>
2023-03-27 22:14:34 +0900

    #23 [Doc, Style] Add 'step__inner__header' element and Add condition that show ProgressBar to StepInner.tsx and Make scss file for StepInner

--- src/components/signUp/StepInner.tsx
+++ src/components/signUp/StepInner.tsx
@@ -1,14 +1,18 @@
-import { ReactNode } from 'react';
+import { ReactNode, useContext } from 'react';
 import NextBtn, { NextBtnProps } from './NextBtn';
 import ProgressBar from './ProgressBar';
+import '../../assets/signUp/stepInner.scss';
+import { SignUpContext } from '../../pages/SignUp';
 type StepInnerProps = NextBtnProps & {
   children: ReactNode;
 };
 const StepInner = ({ children, disableBtn, onClickNextBtn }: StepInnerProps) => {
+  const { signUpState } = useContext(SignUpContext);
   return (
     <div className="step__inner">
+      <div className="step__inner__header">Welcome!</div>
       {children}
-      <ProgressBar />
+      {signUpState.progress !== 'agreeToTerms' && <ProgressBar />}
       <NextBtn disableBtn={disableBtn} onClickNextBtn={onClickNextBtn} />
     </div>
   );
