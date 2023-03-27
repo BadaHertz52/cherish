13556cf084e9ca96c498d0967a204fa5b2f103c7
badahertz52 <badahertz52@gmail.com>
2023-03-27 18:05:13 +0900

    Fix wrong typo

--- src/components/signUp/signUpTypes.ts
+++ src/components/signUp/signUpTypes.ts
@@ -1,12 +1,12 @@
 //⚠️ singUpProgress 순서는 꼭 회원가입 진행 단계 순서과 일치하게 할 것
 const signUpProgress = {
-  agreeToTerms = 'agreeToTerms',
-  nameAndNickName = 'nameAndNickName', // name 이랑 nickname 같이
-  email = 'email',
-  pw = 'pw',
-  genderAndBirth = 'genderAndBirth',
-  job = 'job',
-} as const ;
+  agreeToTerms: 'agreeToTerms',
+  nameAndNickName: 'nameAndNickName', // name 이랑 nickname 같이
+  email: 'email',
+  pw: 'pw',
+  genderAndBirth: 'genderAndBirth',
+  job: 'job',
+} as const;
 export type SignUpProgressType = keyof typeof signUpProgress;
 // 약관 동의란에는 progressBar가 필요 없음
 export const progressArr: SignUpProgressType[] = [
@@ -18,35 +18,34 @@ export const progressArr: SignUpProgressType[] = [
   'job',
 ];
 const inputFormId = {
-  name = 'name',
-  nickName = 'nickName',
-  email = 'email',
-  pw = 'pw',
-  confirmPw = 'confirmPw',
-} as const ;
+  name: 'name',
+  nickName: 'nickName',
+  email: 'email',
+  pw: 'pw',
+  confirmPw: 'confirmPw',
+} as const;
 export type InputFormIdType = keyof typeof inputFormId;
 const sessionData = {
-  ...inputFormId ,
-  job ="job",
-
-} as const ;
-const  gender  = {
-  male = 'male',
-  female = 'female',
-} as const ;
+  ...inputFormId,
+  job: 'job',
+} as const;
+const gender = {
+  male: 'male',
+  female: 'female',
+} as const;
 export type GenderType = keyof typeof gender;
-const  job  = {
-  profession = 'profession', //전문직
-  management = 'management', //경영 관리
-  desk = 'desk', //사무직
-  service = 'service', // 서비스직
-  blueCollar = 'blue-collar', //노동 생산직
-  selfEmployment = 'self-employment', //자영업자
-  student = 'student', //삭생
-  homemaker = 'homemaker', // 가정주부
-  inoccupation = 'inoccupation', // 무직
-  etc = 'etc', //기타
-} as const ;
+const job = {
+  profession: 'profession', //전문직
+  management: 'management', //경영 관리
+  desk: 'desk', //사무직
+  service: 'service', // 서비스직
+  blueCollar: 'blue-collar', //노동 생산직
+  selfEmployment: 'self-employment', //자영업자
+  student: 'student', //삭생
+  homemaker: 'homemaker', // 가정주부
+  inoccupation: 'inoccupation', // 무직
+  etc: 'etc', //기타
+} as const;
 export type JobType = keyof typeof job;
 export type SignUpStateType = {
   progress: SignUpProgressType;
@@ -60,14 +59,14 @@ export type SignUpStateType = {
   birth: { year: number; month: number; date: number } | null;
   job: JobType | null;
 };
-const  errorType = {
-  required = 'required',
-  invalidName = 'invalidName',
-  invalidNickName = 'invalidNickName',
-  invalidEmail = 'invalidEmail',
-  invalidPw = 'invalidPw',
-  invalidConfirmPw = 'invalidConfirmPw',
-} as const ;
+const errorType = {
+  required: 'required',
+  invalidName: 'invalidName',
+  invalidNickName: 'invalidNickName',
+  invalidEmail: 'invalidEmail',
+  invalidPw: 'invalidPw',
+  invalidConfirmPw: 'invalidConfirmPw',
+} as const;
 export type ErrorType = keyof typeof errorType;
 const pass = 'pass';
 export type TestResultType = ErrorType | typeof pass;
