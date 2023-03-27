db64fd054891c79263085ebe7c4097124f76571e
badahertz52 <badahertz52@gmail.com>
2023-03-27 17:58:14 +0900

    #23 [Doc, Style] Modify SingUpTopBar  and Make scss file for it

--- src/components/signUp/SingUpTopBar.tsx
+++ src/components/signUp/SingUpTopBar.tsx
@@ -1,4 +1,39 @@
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useContext } from 'react';
+import { SignUpContext } from '../../pages/SignUp';
+import { progressArr, SignUpStateType } from './signUpTypes';
+import '../../assets/signUp/signUpTopBar.scss';
 const SignUpTopBar = () => {
-  return <div id="signUpTopBar"></div>;
+  const { signUpState, setSignUpState } = useContext(SignUpContext);
+  const saveData = () => {
+    if (signUpState.progress !== 'genderAndBirth' && signUpState.progress !== 'job') {
+      const listOfInputEl = document.getElementsByTagName('input');
+      const dataArr = [...listOfInputEl].map((el: HTMLInputElement) => ({
+        id: el.id,
+        value: el.value,
+      }));
+    }
+  };
+  const onClickPrevBtn = () => {
+    // 현재 작성한 내용 저장 여부는 기획팀과 의논해봐야 함
+    //이전 단계로 이동
+    const currentStepIndex = progressArr.indexOf(signUpState.progress);
+    setSignUpState((prevState: SignUpStateType) => {
+      const newState: SignUpStateType = {
+        ...prevState,
+        progress: progressArr[currentStepIndex - 1],
+      };
+      return newState;
+    });
+  };
+  return (
+    <div id="signUpTopBar">
+      <button className="btn-prev" onClick={onClickPrevBtn} title="btn-prev" type="button">
+        <FontAwesomeIcon icon={faChevronLeft} />
+      </button>
+      <div className="title">간편 가입</div>
+    </div>
+  );
 };
 export default SignUpTopBar;
