29b068427b0b90e0c7433ef0857d832fa81233a3
badahertz52 <badahertz52@gmail.com>
2023-03-29 15:44:46 +0900

    Modify font family

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { Route, Routes } from "react-router-dom";
-import First from "./pages/First";
-import Second from "./pages/Second";
-import Third from "./pages/Third";
-import Fourth from "./pages/Fourth";
-import Main from "./pages/Main";
-import ModalTest from "./components/modals/ModalTest";
-
+import { Route, Routes } from 'react-router-dom';
+import First from './pages/First';
+import Second from './pages/Second';
+import Third from './pages/Third';
+import Fourth from './pages/Fourth';
+import Main from './pages/Main';
+import ModalTest from './components/modals/ModalTest';
+import './assets/main.scss';
 function App() {
   return (
     <div className="App">
@@ -16,7 +16,7 @@ function App() {
         <Route path="/third" element={<Third />} />
         <Route path="/fourth" element={<Fourth />} />
         {/* modal 테스트를 위해 route 추가 , 추후 삭제 예정*/}
-        <Route path="modaltest" element={<ModalTest/>}/>
+        <Route path="modaltest" element={<ModalTest />} />
       </Routes>
     </div>
   );
