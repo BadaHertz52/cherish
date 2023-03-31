e2894639e79a24232065b990190846137fae7628
badahertz52 <badahertz52@gmail.com>
2023-03-31 18:43:06 +0900

    #23 [Doc,Feat] Modify ProgressBar to change its left accroding to value of  diableBtn

--- src/components/signUp/ProgressBar.tsx
+++ src/components/signUp/ProgressBar.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
-import { SignUpProgressType } from './signUpTypes';
+import { SignUpProgressType, progressArr } from './signUpTypes';
 
-export const moveProgressBar = (progress: SignUpProgressType) => {
-  const progressEl = document.querySelector('.progress');
+export const moveProgressBar = (progress: SignUpProgressType, forward: boolean) => {
+  const progressEl = document.querySelector('.progress') as HTMLElement | null;
+  const progressIndex = progressArr.indexOf(progress);
   if (progressEl !== null) {
+    const number: number = forward ? progressIndex + 1 : progressIndex;
+    if (progressIndex === progressArr.length - 1) {
+      progressEl.style.left = '0';
+    } else {
+      const left = `-${((progressArr.length - number) / progressArr.length) * 100}%`;
+      progressEl.style.left = left;
+    }
   }
 };
 const ProgressBar = () => {
