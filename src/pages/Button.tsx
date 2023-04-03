ea777a12c0abfeb747f82d6c6ae32577b45b7676
badahertz52 <badahertz52@gmail.com>
2023-04-03 15:28:31 +0900

    Resolve conflict with upstream

---
+++ src/pages/Button.tsx
@@ -0,0 +1,43 @@
+import Button from '../components/Button';
+import '../assets/Button.scss';
+import axios from 'axios';
+const button = () => {
+  const handleButtonClick = () => {
+    console.log('Button Clicked!');
+  };
+  return (
+    <>
+      <Button onClickAction={handleButtonClick} className="disabled">
+        다음
+      </Button>
+      <Button
+        onClickAction={() => {
+          window.location.href = '/';
+        }}
+        className="abled"
+      >
+        다음
+      </Button>
+      <Button type="submit" className="default">
+        다음
+      </Button>
+      <Button
+        onClickAction={() => {
+          axios
+            .get('https://jsonplaceholder.typicode.com/todos/')
+            .then(reponse => {
+              console.log(reponse.data);
+            })
+            .catch(error => {
+              console.log(error.reponse);
+            });
+        }}
+        className="default2"
+      >
+        다음
+      </Button>
+    </>
+  );
+};
+
+export default button;
