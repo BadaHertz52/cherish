6289b95cc2322cb761ffa03ea05462c5fdc2c8ea
Victor <victor@supertone.ai>
2023-04-03 23:50:44 +0900

    fix: React 18 방식으로 DOM render

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import * as ReactDOMClient from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import 'reset-css';
-ReactDOM.render(
+
+const container = document.getElementById('root') as HTMLElement;
+
+// Create a root.
+const root = ReactDOMClient.createRoot(container);
+
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root'),
 );
