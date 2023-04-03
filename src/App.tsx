75943545c554860d59a6ef0f8dc6c4dc1d5c1521
Victor <victor@supertone.ai>
2023-04-03 22:22:09 +0900

    feat: home tab 일부 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,23 @@
 import { Route, Routes } from 'react-router-dom';
-import First from './pages/First';
 import Second from './pages/Second';
 import Third from './pages/Third';
 import Fourth from './pages/Fourth';
 import Button from './pages/Button';
-import Main from './pages/Main';
+import Home from './pages/Home';
+import BottomNav from './components/BottomNav';
+import './assets/styles/App.scss';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/first" element={<First />} />
+        <Route path="/" element={<Home />} />
         <Route path="/second" element={<Second />} />
         <Route path="/third" element={<Third />} />
         <Route path="/fourth" element={<Fourth />} />
         <Route path="/button" element={<Button />} />
       </Routes>
+      <BottomNav />
     </div>
   );
 }
