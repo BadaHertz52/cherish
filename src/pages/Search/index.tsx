97199f6b817666ec19c3f1bbdd69017c3fd45caa
SeungMin Shin <sky2197kr@gmail.com>
2023-04-06 19:15:06 +0900

    feat: filter UI 구현

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,6 +1,7 @@
 import styles from './style.module.scss';
 import { useState, useEffect } from 'react';
 import { SearchKeywordSection, SearchHeader, ItemCard } from '@/components';
+import FilterDropdownSvg from '@/assets/svgs/filter-dropdown.svg';
 import type { Item } from '@/components/ItemCard';
 
 const SearchPage = () => {
@@ -8,9 +9,19 @@ const SearchPage = () => {
   const [recommendKeywords, setRecommendKeywords] = useState<string[]>([]);
   const [recentProducts, setRecentProducts] = useState<Item[]>([]);
 
+  const [isSearch, setIsSearch] = useState(true);
+
   const handleSearch = (keyword: string) => {
     // TODO: search API
+    setIsSearch(true);
   };
+
+  const filters = [
+    { type: '상품유형', value: ['식품', '뷰티', '리빙/주방', '디지털가전', '의류/잡화', '기타'] },
+    { type: '성별', value: ['식품', '뷰티', '리빙/주방', '디지털가전', '의류/잡화', '기타'] },
+    { type: '직업', value: ['식품', '뷰티', '리빙/주방', '디지털가전', '의류/잡화', '기타'] },
+    { type: '상황', value: ['식품', '뷰티', '리빙/주방', '디지털가전', '의류/잡화', '기타'] },
+  ];
 
   useEffect(() => {
     // TODO: fetch API
@@ -33,18 +44,35 @@ const SearchPage = () => {
     <div className={styles.searchPage}>
       <SearchHeader handleSearch={handleSearch} />
       <section className={styles.contents}>
-        <h2>최근 검색어</h2>
-        <SearchKeywordSection items={recentKeywords} handleSearch={handleSearch} />
+        {!isSearch ? (
+          <>
+            <h2>최근 검색어</h2>
+            <SearchKeywordSection items={recentKeywords} handleSearch={handleSearch} />
 
-        <h2>추천 검색어</h2>
-        <SearchKeywordSection items={recommendKeywords} handleSearch={handleSearch} />
+            <h2>추천 검색어</h2>
+            <SearchKeywordSection items={recommendKeywords} handleSearch={handleSearch} />
 
-        <h2>최근 본 상품</h2>
-        <div className={styles.productSection}>
-          {recentProducts.map(item => (
-            <ItemCard key={item.id} {...item} />
-          ))}
-        </div>
+            <h2>최근 본 상품</h2>
+            <div className={styles.productSection}>
+              {recentProducts.map(item => (
+                <ItemCard key={item.id} {...item} />
+              ))}
+            </div>
+          </>
+        ) : (
+          <>
+            <div className={styles.filters}>
+              {filters.map(filter => (
+                <div className={styles.filter}>
+                  <span>{filter.type}</span>
+                  <FilterDropdownSvg />
+                </div>
+              ))}
+            </div>
+            {/* TODO: 이름 변경하기 */}
+            <h2>가나다님, 이런 선물 어때요?</h2>
+          </>
+        )}
       </section>
     </div>
   );
