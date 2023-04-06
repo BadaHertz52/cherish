f5c1016188caf31da97868dfd347d0eb6f0dca10
SeungMin Shin <sky2197kr@gmail.com>
2023-04-06 16:02:35 +0900

    feat: default export 옵션 추가

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,12 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [
+    react(),
+    svgr({
+      exportAsDefault: true,
+    }),
+  ],
   test: {
     globals: true,
     environment: 'happy-dom',
