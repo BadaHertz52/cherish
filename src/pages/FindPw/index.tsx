bcf7aef9f6e1c93c3d5bd915236a3a422e2803fd
badahertz52 <badahertz52@gmail.com>
2023-04-08 19:03:11 +0900

    #67 [Doc, Style] Modify dom that FindPw return and Make style.module.scss in pages/FindPw

--- src/pages/FindPw/index.tsx
+++ src/pages/FindPw/index.tsx
@@ -5,7 +5,6 @@ import { PasswordForm } from '@/components';
 import { InputDataType, initialInputData } from '../SignUp/signUpTypes';
 
 const FindPw = () => {
-  const btnRef = useRef<HTMLButtonElement>(null);
   // 이메일 인증 여부
   const [openEmailForm, setOpenEmailForm] = useState<boolean>(true);
   const [email, setEmail] = useState<InputDataType>(initialInputData);
@@ -16,36 +15,47 @@ const FindPw = () => {
     //change pw
   };
   return (
-    <div id="find-pw">
-      <h3>회원님의 비밀번호를 찾을 수 있도록 가입하신 이메일을 입력해 주세요.</h3>
-      {openEmailForm ? (
-        <EmailVerification
-          additionOfLabel="가입하신"
-          disableBtn={openEmailForm}
-          setDisableBtn={setOpenEmailForm}
-          email={email}
-          setEmail={setEmail}
-          emailDuplicationChecker={false}
-          toastModalPositionTargetEl={btnRef.current}
-        />
-      ) : (
-        <PasswordForm
-          confirmPw={confirmPw}
-          setConfirmPw={setConfirmPw}
-          pw={pw}
-          setPw={setPw}
-          setDisableBtn={setDisableBtn}
-        />
-      )}
-      <button
-        type="button"
-        ref={btnRef}
-        className="btnChangePw"
-        disabled={disableBtn}
-        onClick={handleClickBtn}
-      >
-        다음
-      </button>
+    <div id="find-pw" className={styles.findPw}>
+      <div className={styles.topBar}>
+        <h2>비밀번호 찾기</h2>
+      </div>
+      <div className={styles.inner}>
+        <h3>
+          <p>회원님의 비밀번호를 찾을 수 있도록</p>
+          <p>가입하신 이메일을 입력해 주세요.</p>
+        </h3>
+        {openEmailForm ? (
+          <EmailVerification
+            additionOfLabel="가입하신"
+            disableBtn={openEmailForm}
+            setDisableBtn={setOpenEmailForm}
+            email={email}
+            setEmail={setEmail}
+            emailDuplicationChecker={false}
+            toastModalPositionTargetEl={null}
+          />
+        ) : (
+          <>
+            <PasswordForm
+              confirmPw={confirmPw}
+              setConfirmPw={setConfirmPw}
+              pw={pw}
+              setPw={setPw}
+              setDisableBtn={setDisableBtn}
+            />
+            <div className={styles.btnContainer}>
+              <button
+                type="button"
+                className={styles.btnChangePw}
+                disabled={disableBtn}
+                onClick={handleClickBtn}
+              >
+                다음
+              </button>
+            </div>
+          </>
+        )}
+      </div>
     </div>
   );
 };
