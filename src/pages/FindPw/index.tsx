6cb3755a63263d475937b19950f28d196b0c5c16
badahertz52 <badahertz52@gmail.com>
2023-04-10 10:40:58 +0900

    #67 Modify FindPw to add btn that move previous step in topBar

--- src/pages/FindPw/index.tsx
+++ src/pages/FindPw/index.tsx
@@ -4,6 +4,9 @@ import styles from './style.module.scss';
 import { EmailVerification } from '@/components';
 import { PasswordForm } from '@/components';
 import { InputDataType, initialInputData } from '../SignUp/signUpTypes';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
+import { useNavigate } from 'react-router-dom';
 
 const FindPw = () => {
   // 이메일 인증 여부
@@ -12,12 +15,29 @@ const FindPw = () => {
   const [pw, setPw] = useState<InputDataType>(initialInputData);
   const [confirmPw, setConfirmPw] = useState<InputDataType>(initialInputData);
   const [disableBtn, setDisableBtn] = useState<boolean>(true);
+  const [openToastModal, setOpenToastModal] = useState<boolean>(false);
+  const navigate = useNavigate();
   const handleClickBtn = () => {
     //change pw
+    // 서버 연동 후
+    //opentoastmodal
+    setTimeout(() => {
+      navigate('/login');
+    }, 2100);
+  };
+  const onClickPrevBtn = () => {
+    if (openEmailForm) {
+      navigate('/login');
+    } else {
+      setOpenEmailForm(true);
+    }
   };
   return (
     <div id="find-pw">
       <div className={styles.topBar}>
+        <button className="btn-prev" onClick={onClickPrevBtn} title="btn-prev" type="button">
+          <FontAwesomeIcon icon={faChevronLeft} />
+        </button>
         <h2>비밀번호 찾기</h2>
       </div>
       <div className={styles.inner}>
@@ -44,14 +64,14 @@ const FindPw = () => {
               setPw={setPw}
               setDisableBtn={setDisableBtn}
             />
-            <div className={styles.btnContainer}>
+            <div className={`${styles.btnContainer} btn-container`}>
               <button
                 type="button"
                 className={styles.btnChangePw}
                 disabled={disableBtn}
                 onClick={handleClickBtn}
               >
-                다음
+                비밀번호 변경
               </button>
             </div>
           </>
