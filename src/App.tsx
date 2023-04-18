b0e1bb77914837b500fee5d06ae431f9378532de
badahertz52 <badahertz52@gmail.com>
2023-04-18 11:08:11 +0900

    Recovery App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
-import { BottomNav } from '@/components';
-import Router from '@/Router';
+import { Route, Routes } from 'react-router-dom';
+
+import { BottomNav } from './components';
+// import ModalTest from './examples/modalTest';
+import Curation from './pages/Curation';
+import PresentRecommendation from './pages/Curation/components/PresentRecommendation';
+import FindPw from './pages/FindPw';
+// import Fourth from './pages/Fourth';
+import Home from './pages/Home';
+import LogIn from './pages/LogIn';
+import SignUp from './pages/SignUp';
+import Third from './pages/Third';
 
 import './assets/main.scss';
 
-function App(): JSX.Element {
+function App() {
   return (
     <div className="App">
-      <Router />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/first" element={<First />} /> */}
+        <Route path="/curation" element={<Curation />} />
+        <Route path="/curation/present/recommendation" element={<PresentRecommendation />} />
+        <Route path="/third" element={<Third />} />
+        {/* <Route path="/fourth" element={<Fourth />} /> */}
+        {/* modal 테스트를 위해 route 추가 , 추후 삭제 예정*/}
+        {/* <Route path="modaltest" element={<ModalTest />} /> */}
+        <Route path="login" element={<LogIn />} />
+        <Route path="signup" element={<SignUp />} />
+        <Route path="findpw" element={<FindPw />} />
+      </Routes>
       <BottomNav />
     </div>
   );
