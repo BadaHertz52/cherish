e85a76170fe6889c233e957e1f287f8a14cebd6e
SeungMin Shin <sky2197kr@gmail.com>
2023-04-20 23:05:23 +0900

    feat: Add custom input for MemberModifyPage

---
+++ src/pages/MemberModify/components/CustomInput/index.tsx
@@ -0,0 +1,41 @@
+import EyeslashSvg from '@/assets/svgs/eyeslash.svg';
+import PasswordCheckSvg from '@/assets/svgs/password-check.svg';
+
+import styles from './style.module.scss';
+
+export type CustomInputProps = {
+  title?: string;
+  type: 'text' | 'password';
+  placeholder?: string;
+  errorMessage?: string;
+  value: string;
+  setValue: (value: string) => void;
+};
+
+export const CustomInput = ({
+  title,
+  value,
+  type,
+  setValue,
+  placeholder,
+  errorMessage,
+}: CustomInputProps) => {
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
+
+  return (
+    <div className={styles.customInput}>
+      <div className={styles.title}>{title}</div>
+      <div className={styles.inputForm}>
+        <input placeholder={placeholder} value={value} onChange={handleInputChange} />
+        {type === 'password' && (
+          <div className={styles.icon}>
+            <PasswordCheckSvg />
+          </div>
+        )}
+      </div>
+      {errorMessage && <div className={styles.errorMessage}>{errorMessage}</div>}
+    </div>
+  );
+};
