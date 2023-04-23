7db3df5a0c0d9c12a3da1fd551361f834291b74d
badahertz52 <badahertz52@gmail.com>
2023-04-23 16:50:52 +0900

    [Refactor] Make AuthNumberForm so Use it in EmailVerification

--- src/components/EmailVerification/index.tsx
+++ src/components/EmailVerification/index.tsx
@@ -1,5 +1,4 @@
 import {
-  ChangeEvent,
   Dispatch,
   ReactNode,
   SetStateAction,
@@ -9,18 +8,11 @@ import {
   useState,
 } from 'react';
 
-import { onAuthNumber, onEmailVerification } from '@/api/auth/email';
-import {
-  AuthNumberAPIParams,
-  APIResult,
-  EMAIL_API_RESULT_TYPE,
-  EmailAPIResultType,
-} from '@/api/auth/types';
-import { Timer, AlertModal, InputForm, ToastModal } from '@/components';
+import { onEmailVerification } from '@/api/auth/email';
+import { EMAIL_API_RESULT_TYPE, EmailAPIResultType } from '@/api/auth/types';
+import { AlertModal, InputForm, ToastModal } from '@/components';
 import { getToastModalPosition } from '@/components/Modals/functions';
-import { XSSCheck } from '@/pages/LogIn/index';
 import {
-  ERROR_MSG,
   INPUT_FORM_ID,
   InputDataType,
   InputFormIdType,
@@ -28,6 +20,7 @@ import {
 } from '@/pages/SignUp/signUpTypes';
 
 import './style.scss';
+import AuthNumberForm from '../AuthNumberForm';
 import { ToastModalType } from '../Modals/modalTypes';
 
 type EmailVerificationProps = {
@@ -121,54 +114,7 @@ const EmailVerification = ({
       console.error(error);
     }
   };
-  const onChangeAuthNumber = (event: ChangeEvent<HTMLInputElement>) => {
-    const text = XSSCheck(event.target.value, undefined);
-    setAuthNumber({
-      value: text,
-    });
-  };
-  const onClickAuthNumberBtn = async () => {
-    //백엔드에 이메인 인증 번호 확인
-    const params: AuthNumberAPIParams = {
-      email: email.value,
-      code: authNumber.value,
-    };
-    const result: APIResult = await onAuthNumber(params);
-    //data는  string type으로
-    if (result.success) {
-      authNumber.errorType &&
-        setAuthNumber(prev => ({
-          ...prev,
-          errorType: undefined,
-        }));
-      //서버에서 받은 인증 번호와 사용자가 입력한 인증 번호가 일치할 경우
-      setOpenTimer(false);
-      verifiedEmail.current = email.value;
-      setOpenToastModal(true);
-      if (isInFindPw) {
-        setTimeout(() => {
-          setDisableBtn(false);
-          setOpenToastModal(false);
-        }, 1000);
-      } else {
-        setDisableBtn(false);
-      }
-    } else {
-      //인증 번호 불일치
-      setAuthNumber(prev => ({
-        ...prev,
-        errorType: 'invalidAuthNumber',
-      }));
-    }
-  };
-  const handleBlurOfAuthNumber = () => {
-    if (!authNumber.value) {
-      setAuthNumber({
-        ...authNumber,
-        errorType: 'required',
-      });
-    }
-  };
+
   //이메일 인증 5분간 중단/ 하루 인증 횟수 초과 시 , 이메일 작성 폼으로 돌아감
   const onClickCloseBtnInAlertModal = () => {
     setOpenAlertModal(false);
@@ -243,44 +189,19 @@ const EmailVerification = ({
         )}
       </section>
       {openAuthNumberForm && (
-        <section className="authNumber-form">
-          <label htmlFor="authNumber">인증 번호</label>
-          <div className="authNumber-form__contents">
-            <input
-              id="authNumber"
-              name="authNumber"
-              value={authNumber.value}
-              onChange={onChangeAuthNumber}
-              onBlur={handleBlurOfAuthNumber}
-              placeholder="인증번호(6자리)"
-            />
-            {openTimer && <Timer setOpenTimer={setOpenTimer} setOverTime={setOverTime} />}
-            <button
-              disabled={!(openTimer && authNumber.value)}
-              className="btn-authNumber"
-              type="button"
-              onClick={onClickAuthNumberBtn}
-            >
-              확인
-            </button>
-          </div>
-
-          <div className="msg">
-            {overTime ? (
-              <p className="msg-over-time">인증 시간이 지났습니다.</p>
-            ) : authNumber.errorType ? (
-              <div className="error-msg">
-                <p>{ERROR_MSG[authNumber.errorType]}</p>
-                <p>인증번호를 다시 확인해주세요.</p>
-              </div>
-            ) : (
-              <div className="alert">
-                <p>이메일이 수신되지 않는 경우, 입력하신 이메일이 정확한지 확인해 주세요.</p>
-                <p>또는 스펨메일함과 메일함 용량을 확인해 주세요.</p>
-              </div>
-            )}
-          </div>
-        </section>
+        <AuthNumberForm
+          email={email}
+          authNumber={authNumber}
+          setAuthNumber={setAuthNumber}
+          openTimer={openTimer}
+          setOpenTimer={setOpenTimer}
+          overTime={overTime}
+          setOverTime={setOverTime}
+          setOpenToastModal={setOpenToastModal}
+          setDisableBtn={setDisableBtn}
+          verifiedEmail={verifiedEmail}
+          isInFindPw={isInFindPw}
+        />
       )}
       {openAlertModal && (
         <AlertModal center={true} short={true} closeModal={onClickCloseBtnInAlertModal}>
