42d83747ce4f721085f20bc6e81cf07dda6eacf8
badahertz52 <badahertz52@gmail.com>
2023-04-25 14:59:53 +0900

    Upate api/index.ts accroding to main of upstream

--- src/api/index.ts
+++ src/api/index.ts
@@ -5,7 +5,7 @@ export type APIErrorData = {
 };
 
 export const httpClient = axios.create({
-  baseURL: import.meta.env.VITE_SERVER_API_URL,
+  baseURL: import.meta.env.PROD ? import.meta.env.VITE_SERVER_API_URL : '/',
 });
 
 export const handleAxiosError = (axiosError: AxiosError) => {
